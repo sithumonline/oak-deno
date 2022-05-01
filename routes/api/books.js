f2866a470d1e1fc1979577de2f3b50f0beb72b99
Sithum Bopitiya <sithumsandeepap@gmail.com>
2022-05-01 16:55:40 +0530

    Add joi validator

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,6 +1,7 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 export const bookRouter = new Router();
 import { query } from "../../config/db.js";
+import joi from "https://cdn.skypack.dev/joi";
 
 // @route GET api/books/test
 // @description tests books route
@@ -23,11 +24,19 @@ bookRouter.get("/", async (ctx) => {
 // @access Public
 bookRouter.get("/:id", async (ctx) => {
   const id = ctx.params.id;
+  const { error } = joi
+    .object({ id: joi.string().guid().required() })
+    .validate({ id });
+  if (error) {
+    ctx.response.status = 400;
+    ctx.response.body = { msg: error.message };
+    return;
+  }
   const data = await query({
     text: "SELECT * FROM book WHERE id = $id",
     args: {
       id: id,
-    }
+    },
   });
   ctx.response.body = { msg: data ? data.rows : "No Book found" };
   ctx.response.status = data ? 200 : 404;
@@ -37,11 +46,24 @@ bookRouter.get("/:id", async (ctx) => {
 // @description add/save book
 // @access Public
 bookRouter.post("/", async (ctx) => {
+  const body = await ctx.request.body();
+  const data = await body.value;
+  const { error } = joi
+    .object({
+      title: joi.string().required(),
+      author: joi.string().required(),
+      description: joi.string().required(),
+    })
+    .validate(data);
+  if (error) {
+    ctx.response.status = 400;
+    ctx.response.body = { msg: error.message };
+    return;
+  }
   if (ctx.request.hasBody) {
-    const body = await ctx.request.body();
-    const data = await body.value;
     const out = await query({
-      text: "INSERT INTO book (title, author, description, updated_date) VALUES ($title, $author, $description, $updated_date)",
+      text:
+        "INSERT INTO book (title, author, description, updated_date) VALUES ($title, $author, $description, $updated_date)",
       args: {
         ...data,
         updated_date: new Date(),
@@ -63,8 +85,22 @@ bookRouter.put("/:id", async (ctx) => {
     const id = ctx.params.id;
     const body = await ctx.request.body();
     const data = await body.value;
+    const { error } = joi
+      .object({
+        id: joi.string().guid().required(),
+        title: joi.string().required(),
+        author: joi.string().required(),
+        description: joi.string().required(),
+      })
+      .validate({ id, ...data });
+    if (error) {
+      ctx.response.status = 400;
+      ctx.response.body = { msg: error.message };
+      return;
+    }
     const out = await query({
-      text: "UPDATE book SET title = $title, author = $author, description = $description, updated_date = $updated_date WHERE id = $id",
+      text:
+        "UPDATE book SET title = $title, author = $author, description = $description, updated_date = $updated_date WHERE id = $id",
       args: {
         ...data,
         id: id,
@@ -81,6 +117,14 @@ bookRouter.put("/:id", async (ctx) => {
 // @access Public
 bookRouter.delete("/:id", async (ctx) => {
   const id = ctx.params.id;
+  const { error } = joi
+    .object({ id: joi.string().guid().required() })
+    .validate({ id });
+  if (error) {
+    ctx.response.status = 400;
+    ctx.response.body = { msg: error.message };
+    return;
+  }
   const out = await query({
     text: "DELETE FROM book WHERE id = $id",
     args: {
